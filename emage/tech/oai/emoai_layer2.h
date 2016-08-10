706c5cf9eb510b470a73badd7e455612aa2aa866
herlesupreeth <herlesupreeth@gmail.com>
2016-08-10 15:58:21 +0200

    Implemented the logic to extract Layer 2 statistics from OAI stack and provide to Emage agent

---
+++ emage/tech/oai/emoai_layer2.h
@@ -0,0 +1,90 @@
+/* Copyright (c) 2016 Supreeth Herle <s.herle@create-net.org>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ */
+
+/* OAI Layer 2 related technology abstractions for emage.
+ *
+ * This element implements OAI Layer 2 related technologies, and provides
+ * service (statistics) to the agent.
+ *
+ */
+
+#ifndef __EMOAI_LAYER2_H
+#define __EMOAI_LAYER2_H
+
+#include <main.pb-c.h>
+#include <statistics.pb-c.h>
+#include <emlog.h>
+
+/* UE identifier template. */
+typedef int ueid_t;
+/* Module identifier template. */
+typedef uint32_t mid_t;
+
+/* Refers to the layer 2 statistics request and prepares the requested layer 2
+ * statistics reply.
+ *
+ * Returns 0 on success, or a negative error code on failure.
+ */
+int emoai_L2_stats_reply (EmageMsg * request, EmageMsg ** reply);
+
+/* Prepares the buffer status report for layer 2
+ * statistics reply.
+ *
+ * Returns 0 on success, or a negative error code on failure.
+ */
+int emoai_prep_bsr (size_t n_bsr, uint32_t ** bsr, mid_t m_id, ueid_t ue_id);
+
+/* Prepares the rlc buffer status report for layer 2
+ * statistics reply.
+ *
+ * Returns 0 on success, or a negative error code on failure.
+ */
+int emoai_prep_rlc_bsr (size_t n_rlc_bsr,
+						RlcBsrReport ***buff_stat_reports,
+						mid_t m_id,
+						ueid_t ue_id);
+
+/* Prepares the downlink CQI report for layer 2
+ * statistics reply.
+ *
+ * Returns 0 on success, or a negative error code on failure.
+ */
+int emoai_prep_dl_cqi (CsiType type,
+					   DlCqiReport **dl_cqi_report,
+					   mid_t m_id,
+					   ueid_t ue_id);
+
+/* Prepares the paging buffer status report for layer 2
+ * statistics reply.
+ *
+ * Returns 0 on success, or a negative error code on failure.
+ */
+int emoai_prep_pbsr (PagingBufferReport **paging_buff_stats_report,
+					 mid_t m_id,
+					 ueid_t ue_id);
+
+/* Prepares the uplink CQI report for layer 2
+ * statistics reply.
+ *
+ * Returns 0 on success, or a negative error code on failure.
+ */
+int emoai_prep_ul_cqi (uint32_t type,
+					   UlCqiReport **uplink_cqi_report,
+					   mid_t m_id,
+					   ueid_t ue_id);
+
+/* Prepares the noise interference report for layer 2
+ * statistics reply.
+ *
+ * Returns 0 on success, or a negative error code on failure.
+ */
+int emoai_prep_noise_interf (NoiseInterferenceReport **noise_interf_report,
+							 mid_t m_id,
+							 int CC_id);
+
+#endif
